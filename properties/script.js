838f815d0b51ea2c9bee1f4924cf93734322b938
AmitSingh <114228623+Airstriker123@users.noreply.github.com>
2024-03-05 23:16:32 +1100

    Add files via upload

---
+++ properties/script.js
@@ -0,0 +1,17 @@
+function changeColor() {
+          
+  var footerText = document.getElementById("footer-text");
+  
+  if (footerText.style.color === "white") {
+   
+    footerText.style.color = "#62ff00";
+  
+  } else {
+  
+    footerText.style.color = "white";
+  
+  }
+}
+setInterval(changeColor, 500);
+
+console.log*("-_- (skull emoji)");
